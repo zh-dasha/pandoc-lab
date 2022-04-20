bf448a6d7b4cba8fab6366ac72c9743fdb486253
koch-econ <43249790+koch-econ@users.noreply.github.com>
2022-04-20 07:40:14 +0300

    first upload

---
+++ get_data.py
@@ -0,0 +1,8 @@
+def get_categories(*args, **kwargs):
+    # args and kwargs are for future project development
+    yield (1,"Климатическая техника")
+    yield (2,"Красота и здоровье")
+    yield (3,"Садовая техника")
+    yield (4,"Техника для дома")
+    yield (5,"Техника для кухни")
+    yield (6,"Крупная бытовая техника")
