2f5b2b9a0de369cf735394b61638b663fd71c561
Ilya Kochegin (econ.msu) <koch@econ.msu.ru>
2022-04-20 18:07:03 +0300

    template for py

---
+++ pres_template_md.py
@@ -0,0 +1,15 @@
+slide_md="""## {model}
+:::::::::::::: {.columns}
+::: {.column }
+* вендор: {vendor}
+* модель: {model}
+* артикул: {partno} 
+* цена: {price}
+:::
+::: {.column }
+
+![фото товара]({partno}.png)
+:::
+::::::::::::::
+EOF
+"""
